4101c8121ba4cae13e24d1942777854a3807c31b
leolivier <git@levillain.ml>
2022-02-13 17:37:15 +0100

    possible to set target

--- tests/build-mailu.sh
+++ tests/build-mailu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#[ -n "$1" ] && export target="-- set *.target=$1"
-docker buildx bake -f tests/build.hcl --progress plain --push $1
+cd $(dirname $0)/..
+[ -n "$2" ] && export target="-- set *.target=$2"
+docker buildx bake -f ./tests/build.hcl --progress plain --push $1
