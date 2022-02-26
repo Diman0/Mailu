8b8f1757fa0e66238cd56efb1b8bec01286b55a7
leolivier <git@levillain.ml>
2022-02-26 18:30:06 +0100

    better CLI management

--- tests/build-mailu.sh
+++ tests/build-mailu.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+function usage() {
+  echo $0 '[-p platform(s)] [-i "image list"] [-n] [-l]'
+  echo "-p comma separated list of platforms. default=linux/amd64,linux/arm64,linux/arm/v7"
+  echo "-i images to be built. default=all"
+  echo "-n no cache"
+  echo "-l load only, don't push to docker registry"
+  exit 1
+}
+
+IMAGES=""
+PLATFORMS=""
+NOCACHE=""
+LOAD="--push"
+
+while getopts ":hp:i:nl" flag
+do
+  case "${flag}" in
+    h) usage;;
+    p) PLATFORMS='--set *.platform='"${OPTARG}";;
+    i) IMAGES="${OPTARG}";;
+    n) NOCACHE="--no-cache";;
+    l) LOAD="--load";;
+    *) usage;;
+  esac
+done
+
 cd $(dirname $0)/..
-[ -n "$2" ] && export target="-- set *.target=$2"
-docker buildx bake -f ./tests/build.hcl --progress plain --push $1
+docker run --privileged --rm tonistiigi/binfmt --install all
+set -x
+docker buildx bake -f ./tests/build.hcl --progress plain $NOCACHE $LOAD $PLATFORMS $IMAGES
